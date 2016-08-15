5f545e7396a323985a78367bcf238db00de2bed8
Жека <@DESKTOP-1BN4QJF>
2016-08-15 20:12:33 +0300

    feat (MTI_Studi_2016): Create the first class on Java

---
+++ HomeWork/src/HomeWork1.java
@@ -0,0 +1,173 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class HomeWork1 {
+
+	public static void main(String[] args) {
+
+		System.out.println("---------------------------------------");
+		System.out.println("--------------«адание 1----------------");
+		System.out.println("---------------------------------------");
+		System.out.println("");
+
+		int a = 0, b = 0, c = 0;
+		BufferedReader br;
+
+		try {
+
+			System.out.println("¬ведите значение а");
+			br = new BufferedReader(new InputStreamReader(System.in));
+			a = Integer.parseInt(br.readLine());
+
+			System.out.println("¬ведите значение b");
+			br = new BufferedReader(new InputStreamReader(System.in));
+			b =  Integer.parseInt(br.readLine());
+
+			System.out.println("¬ведите значение c");
+			br = new BufferedReader(new InputStreamReader(System.in));
+			c = Integer.parseInt(br.readLine());
+
+			//System.out.println("¬ведены следующие значени€ а = " + a + " b = " + b + " c = " + c);
+
+			//решаем квадратное уравнение
+			System.out.println(squareEquation(a, b, c));
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+
+		System.out.println("");
+		System.out.println("---------------------------------------");
+		System.out.println("--------------«адание 2----------------");
+		System.out.println("---------------------------------------");
+
+		//выводим все простые числа в промежутке от 1 до 1000
+		printSimpleNumbers(1000);
+
+		System.out.println("");
+		System.out.println("---------------------------------------");
+		System.out.println("--------------«адание 3----------------");
+		System.out.println("---------------------------------------");
+
+		//извлекаем квадратный корень из числа
+
+		long l = 0;
+		long root = 0;
+
+		try {
+			System.out.println("¬ведите число дл€ расчета квадратного корн€");
+			br = new BufferedReader(new InputStreamReader(System.in));
+			l = Long.parseLong(br.readLine());
+
+			root = squareRoot(l);
+
+			if(root*root < l) {
+				System.out.println("Ѕлижайшее целое значение квадратного корн€ числа " + l
+						+ " равн€етс€ " + root);
+			} else {
+				System.out.println("«начение квадратного корн€ числа " + l
+						+ " равн€етс€ " + root);
+			}
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	/*
+	 * ћетод определ€ет простое число или нет
+	 */
+
+	public static boolean isSimpleNumber(long num) {
+
+		long delCount = 0; //количество делителей
+		//идем по всем делител€м введенного числа
+		for(long i = 1; i <= num; i++) {
+			//если число делитс€ без остатка, то увеличиваем счетчик на 1
+			if(num%i == 0) {
+				delCount++;
+			}
+		}
+		//если делителей не больше 2, т.е. 1 и введенное число, то число простое
+		if(delCount <= 2) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	/*
+	 * ¬ыводит все простые числа в диапазоне от 1 до maxnumber
+	 */
+
+	public static void printSimpleNumbers(long maxnumber) {
+		for(int i = 1; i <= maxnumber; i++) {
+			if(isSimpleNumber(i)) {
+				System.out.print(i + " ");
+			}
+		}
+	}
+
+	/*
+	 * ѕодсчитывает квадратный корень целого цисла сложением и вычитанием
+	 */
+
+	public static long squareRoot(long num) {
+		long i = 0;
+
+		//подбираем число, квадрат которого меньше или равен введенному числу
+		while(degree(i+1) <= num) {
+			i++;
+		}
+		//возвращаем ближайший целый корень введенного числа
+		return i;
+	}
+
+
+	/*
+	 * возводит число в степень сложением
+	 */
+	private static long degree(long num) {
+		long result = 0;
+
+		for(long i = 0; i < num; i++) {
+			result = result + num;
+		}
+
+		return result;
+	}
+
+	/*
+	 * решаем квадратное уравнение
+	 */
+	public static String squareEquation(int a, int b, int c) {
+
+		//провер€ем значение а. ≈сли 0, то возвращаем текст с ошибкой
+		if(a == 0) {
+			return "ѕервый коэффициент не может быть 0";
+		}
+
+		String result = "";
+
+		//считаем дискриминант
+		int d = b*b - 4*a*c;
+
+		//System.out.println("ƒискриминант " + d);
+
+		//в зависимости от значени€ дискриминанта выводим решени€ уравнени€
+		if(d == 0) {
+			result = "”равнение имеет 2 одинаковых корн€ x1 = x2 = " + (-(b/2*a));
+		} else if (d < 0) {
+			result = "Ќет дейтвительных решений уравнени€";
+		} else {
+			result = "”равнение имеет 2 корн€ х1 = " + ((-b - Math.sqrt(d))/(2*a)) + " x2 = " +
+					+((-b + Math.sqrt(d))/(2*a));
+		}
+
+		return result;
+	}
+
+
+
+}
