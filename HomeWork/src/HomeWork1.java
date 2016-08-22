44a49c97fc301329d65258f4770c11a764609f34
Евгений Белых <jonik2004@bk.ru>
2016-08-22 10:50:26 +0300

    feat (MTI_Study_2016) Fix some mistakes

--- HomeWork/src/HomeWork1.java
+++ HomeWork/src/HomeWork1.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class HomeWork1 {
@@ -11,53 +12,39 @@ public class HomeWork1 {
 		System.out.println("");
 
 		int a = 0, b = 0, c = 0;
-		BufferedReader br;
-
+		
+		InputStreamReader inputStreamReader = null;
+		BufferedReader br = null;
+		
+		long l = 0;
+		long root = 0;
+		
 		try {
-
+			
+			inputStreamReader = new InputStreamReader(System.in);
+			br = new BufferedReader(inputStreamReader);
+			
 			System.out.println("¬ведите значение а");
-			br = new BufferedReader(new InputStreamReader(System.in));
 			a = Integer.parseInt(br.readLine());
 
 			System.out.println("¬ведите значение b");
-			br = new BufferedReader(new InputStreamReader(System.in));
-			b =  Integer.parseInt(br.readLine());
+			b = Integer.parseInt(br.readLine());
 
 			System.out.println("¬ведите значение c");
-			br = new BufferedReader(new InputStreamReader(System.in));
 			c = Integer.parseInt(br.readLine());
-
-			//System.out.println("¬ведены следующие значени€ а = " + a + " b = " + b + " c = " + c);
 
 			//решаем квадратное уравнение
 			System.out.println(squareEquation(a, b, c));
+			
+			System.out.println("");
+			System.out.println("---------------------------------------");
+			System.out.println("--------------«адание 3----------------");
+			System.out.println("---------------------------------------");
 
-		} catch (Exception e) {
-			System.out.println(e.toString());
-		}
+			//извлекаем квадратный корень из числа
 
-		System.out.println("");
-		System.out.println("---------------------------------------");
-		System.out.println("--------------«адание 2----------------");
-		System.out.println("---------------------------------------");
-
-		//выводим все простые числа в промежутке от 1 до 1000
-		printSimpleNumbers(1000);
-
-		System.out.println("");
-		System.out.println("---------------------------------------");
-		System.out.println("--------------«адание 3----------------");
-		System.out.println("---------------------------------------");
-
-		//извлекаем квадратный корень из числа
-
-		long l = 0;
-		long root = 0;
-
-		try {
 			System.out.println("¬ведите число дл€ расчета квадратного корн€");
-			br = new BufferedReader(new InputStreamReader(System.in));
-			l = Long.parseLong(br.readLine());
+			l = Long.parseLong(br.readLine());		
 
 			root = squareRoot(l);
 
@@ -68,10 +55,30 @@ public class HomeWork1 {
 				System.out.println("«начение квадратного корн€ числа " + l
 						+ " равн€етс€ " + root);
 			}
-
+			
 		} catch (Exception e) {
 			System.out.println(e.toString());
+		} 
+		
+		finally {
+			try {
+				br.close();
+				inputStreamReader.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			
 		}
+
+		System.out.println("");
+		System.out.println("---------------------------------------");
+		System.out.println("--------------«адание 2----------------");
+		System.out.println("---------------------------------------");
+		System.out.println("");
+		
+		//выводим все простые числа в промежутке от 1 до 1000
+		printSimpleNumbers(1000);
+
 	}
 
 	/*
@@ -152,8 +159,6 @@ public class HomeWork1 {
 
 		//считаем дискриминант
 		int d = b*b - 4*a*c;
-
-		//System.out.println("ƒискриминант " + d);
 
 		//в зависимости от значени€ дискриминанта выводим решени€ уравнени€
 		if(d == 0) {
